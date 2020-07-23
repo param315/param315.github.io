39dbfc0d5f4ebdebdbd8e30e7d71e0a45587425a
Mehak Veer <55867338+Mehakveer@users.noreply.github.com>
2020-07-23 10:16:16 -0800

    update

--- script.js
+++ script.js
@@ -10,3 +10,29 @@
     }
   }
 })(window);
+
+//speakhello
+(function (window) {
+var helloSpeaker = {};
+
+var speakWord = "Hello";
+
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+//speakgoodbye
+(function (window) {
+var byeSpeaker = {};
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
