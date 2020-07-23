ffc5fc41b3452c99ef7562baf315bc035edfb283
Mehak Veer <55867338+Mehakveer@users.noreply.github.com>
2020-07-23 10:18:13 -0800

    update

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-(function (window){
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-  for(var i=0;i<names.length;i++){
-    var initLetter = (names[i].charAt(0)).toLowerCase();
-    if(initLetter === 'j'){ 
-      byeSpeaker.speak(names[i]);
-    }else{
-      helloSpeaker.speak(names[i]);
-    }
-  }
-})(window);
-
 //speakhello
 (function (window) {
 var helloSpeaker = {};
@@ -36,3 +23,21 @@ byeSpeaker.speak = function (name) {
 window.byeSpeaker = byeSpeaker;
 
 })(window);
+
+
+
+(function (window){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for(var i=0;i<names.length;i++){
+    var initLetter = (names[i].charAt(0)).toLowerCase();
+    if(initLetter === 'j'){ 
+      byeSpeaker.speak(names[i]);
+    }else{
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})(window);
+
+
+
